d128222411039782f7163c2ffbddd2bcaa4c59cc
SpaceCowboy-FM <140547931+SpaceCowboy-FM@users.noreply.github.com>
2023-09-06 13:39:38 -0500

    Declaration, invocation, and more project

---
+++ declaration_invocation/index.js
@@ -0,0 +1,20 @@
+//no parameter function greeting
+function greeting() {
+    alert( 'Welcome! + ¡Bienvenidos!')
+}
+greeting();
+
+//parameter function greeting
+// function greeting() {
+//   let userName = "Felix";
+//   let greet = "Salutations, " + userName;
+//   alert(greet);
+// }
+// greeting();
+
+//2 number parameter function, return sum
+// function addition(a, b) {
+//     return a + b;
+// }
+// let add = addition(3, 5);
+// console.log(add);
