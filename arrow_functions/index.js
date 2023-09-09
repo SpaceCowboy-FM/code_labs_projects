215995d67887defb4d2eee831e2ec611e8dbbc8f
SpaceCowboy-FM <140547931+SpaceCowboy-FM@users.noreply.github.com>
2023-09-09 17:36:43 -0500

    mastering basic arrow functions in JavaScript

---
+++ arrow_functions/index.js
@@ -0,0 +1,19 @@
+// A function that accepts two arguments and returns their product.
+/*
+let multiply = (a, b) => a * b;
+
+alert(multiply(3, 5));
+*/
+
+// A function that takes in a string and returns it in uppercase.
+/*
+const toUpperCase = (str) => str.toUpperCase();
+
+const upper = toUpperCase("hello");
+console.log(upper);
+*/
+
+// A function with a single parameter, calculating the square of the provided number.
+
+let square = (n) => n * n;
+alert(square(4));
